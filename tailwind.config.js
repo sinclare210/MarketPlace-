8c9cfb44e1646c2c79a9a1059b3d5ba554341ff7
Sinclair <sinclairolajuwon@gmail.com>
2024-10-23 09:31:03 -0500

    changing to prestyled

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
+  content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-       fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
+      fontFamily: {
+        primary: ['Poppins', 'sans-serif'], // Always add a fallback font
+      },
+      container: {
+        padding: {
+          DEFAULT: "30px",
+          lg: "0",
+        },
+      },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1440px",
+      },
+      colors: {
+        primary: "#222222",
+        secondary: "#F5E6ED",
       },
     },
   },
   plugins: [],
 }
-
