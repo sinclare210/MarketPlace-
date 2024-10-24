079908b563db632ef5dd3664bccece27d9e6668f
Sinclair <sinclairolajuwon@gmail.com>
2024-10-24 12:24:11 -0500

    progress

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <Footer className="bg-primary py-12">
+    <footer className="bg-primary py-12">
       <div className='container mx-auto'>
         <p className='text-white text-center'>
           Copyright &copy Ecommerce Shop 2022. All rights reserved.
         </p>
       </div>
-    </Footer>
+    </footer>
   )
 }
 
