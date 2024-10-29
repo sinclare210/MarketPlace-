e34f80091169c8fa50b97f1ee5cdb9d416fdbff5
Sinclair <sinclairolajuwon@gmail.com>
2024-10-29 06:07:02 -0500

    finishing touches

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -7,7 +7,10 @@ const Signup = () => {
   const navigate = useNavigate();
 
   const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+    setFormData({ 
+      ...formData, 
+      [e.target.name]: e.target.value 
+    });
   };
 
   const handleSubmit = (e) => {
